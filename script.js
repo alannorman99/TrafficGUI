53744737bf5eb8a5e6e21aae3a0dbf53649e47e6
Alan Norman <asnorman3604@eagle.fgcu.edu>
2020-11-22 17:23:58 -0500

    Creates map of estero and adds traffic levels on main roads TODO:  add a javascript form for entering a location and anything else/ implement a basic login/logout/create account screen

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
-// Map options
-function initMap() {
-	var options = {
-		zoom:13,
-		center:{lat:27.6648, lng:-81.5158}
-	}
-	// New map
-	var map = new google.maps.Map(document.getElementById('map'), options);
-	addMarker({coords:{lat:26.6406, lng:-81.8723}, content:'<h1>Fort Myers</h1>'}); //FM
-	addMarker({coords:{lat:26.4381, lng:-81.8068}, content:'<h1>Estero</h1>'}); //Estero
-	addMarker({coords:{lat:26.4637, lng:-81.7753}, content:'<h1>FGCU</h1>'}); //FGCU
+window.onload = function () {
+	L.mapquest.key = 'cqknkkaMme9j37I5pUmC1ypE9pLVfozR';
 
-	// Add Marker function
-	function addMarker(props){
-	var marker = new google.maps.Marker({
-	position:props.coords,
-	map:map
+	var map = L.mapquest.map('map', {
+		center: [26.471196, -81.810648],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14
 	});
-	// Check content
-	if (props.content){
-		var infoWindow = new google.maps.InfoWindow({
-		content:props.content
-	});
-	marker.addListener('click', function(){
-		infoWindow.open(map, marker);
-	});
-	}
-   }
+
+	//map.addControl(L.mapquest.control());
+	map.addLayer(L.mapquest.trafficLayer());
+	//map.addLayer(L.mapquest.incidentsLayer());
 }
