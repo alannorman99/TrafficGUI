718dbebe5aeecb3f4283112bb0b48b851e306f93
Alan Norman <asnorman3604@eagle.fgcu.edu>
2020-11-22 20:41:49 -0500

    Added search box and ability to select a new location and pan the map to that location

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-window.onload = function () {
-	L.mapquest.key = 'cqknkkaMme9j37I5pUmC1ypE9pLVfozR';
 
-	var map = L.mapquest.map('map', {
-		center: [26.471196, -81.810648],
-		layers: L.mapquest.tileLayer('map'),
-		zoom: 14
-	});
+L.mapquest.key = 'cqknkkaMme9j37I5pUmC1ypE9pLVfozR';
 
-	//map.addControl(L.mapquest.control());
-	map.addLayer(L.mapquest.trafficLayer());
-	//map.addLayer(L.mapquest.incidentsLayer());
-}
+var map = L.mapquest.map('map', {
+	center: [41.3423, -81.3423],
+	layers: L.mapquest.tileLayer('map'),
+	zoom: 14
+});
+
+map.addControl(L.mapquest.control());
+//map.addLayer(L.mapquest.trafficLayer());
+//map.addLayer(L.mapquest.incidentsLayer());
+
+
+var placeSearch = placeSearch({
+	key: 'cqknkkaMme9j37I5pUmC1ypE9pLVfozR',
+	container: document.querySelector('#place-search-input')
+});
+
+placeSearch.on('change', (e) => {
+
+	console.log(e);
+	const lat = e.result.latlng.lat;
+	const lon = e.result.latlng.lng;
+
+	map.panTo({ lat: lat, lon: lon })
+
+
+});
+
+
